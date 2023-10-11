ed8edf864afbeac84a534bce5be135e01b0add6d
Hubert-cumt <16214911@cumt.edu.cn>
2023-10-12 00:59:51 +0800

    update

--- exercises/intro/intro1.rs
+++ exercises/intro/intro1.rs
@@ -1,9 +1,5 @@
 // intro1.rs
 //
-// About this `I AM NOT DONE` thing:
-// We sometimes encourage you to keep trying things on a given exercise, even
-// after you already figured it out. If you got everything working and feel
-// ready for the next exercise, remove the `I AM NOT DONE` comment below.
 //
 // If you're running this using `rustlings watch`: The exercise file will be
 // reloaded when you change one of the lines below! Try adding a `println!`
