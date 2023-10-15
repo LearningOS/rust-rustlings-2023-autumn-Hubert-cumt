d9fadf91934b4d7d72fe258677417e5112a2d57b
Hubert-cumt <16214911@cumt.edu.cn>
2023-10-16 00:53:03 +0800

    update

--- exercises/generics/generics1.rs
+++ exercises/generics/generics1.rs
@@ -6,8 +6,6 @@
 // Execute `rustlings hint generics1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
     let mut shopping_list: Vec<&str> = Vec::new();
     shopping_list.push("milk");
