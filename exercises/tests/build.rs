5159fefddc9d213f50820840927888d1dca0e790
Hubert-cumt <16214911@cumt.edu.cn>
2023-10-17 01:46:59 +0800

    update

--- exercises/tests/build.rs
+++ exercises/tests/build.rs
@@ -10,15 +10,20 @@ fn main() {
         .duration_since(std::time::UNIX_EPOCH)
         .unwrap()
         .as_secs(); // What's the use of this timestamp here?
-    let your_command = format!(
-        "Your command here with {}, please checkout exercises/tests/build.rs",
-        timestamp
-    );
-    println!("cargo:{}", your_command);
+
+    let test_foo = (timestamp - 3).to_string();
+    println!("cargo:rustc-env=TEST_FOO={}", test_foo);
+
+    // let your_command = format!(
+    //     "Your command here with {}, please checkout exercises/tests/build.rs",
+    //     timestamp
+    // );
+    // println!("cargo:{}", your_command);
 
     // In tests8, we should enable "pass" feature to make the
     // testcase return early. Fill in the command to tell
     // Cargo about that.
-    let your_command = "Your command here, please checkout exercises/tests/build.rs";
-    println!("cargo:{}", your_command);
+    // let your_command = "Your command here, please checkout exercises/tests/build.rs";
+    // println!("cargo:{}", your_command);
+    println!("cargo:rustc-cfg=feature=\"pass\"");
 }
